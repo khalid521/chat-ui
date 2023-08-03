b6cf41ec5274857e9eef14e69fc3fb5bdc276d52
Khalid <khalid521@gmail.com>
2023-08-03 22:15:25 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vite";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
+	server: {
+		port: 3030,
+	  },
+	  preview: {
+		port: 8080,
+	  },
 	plugins: [
 		sveltekit(),
 		Icons({
